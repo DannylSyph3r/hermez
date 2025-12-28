4fbb2e90887fd99d1f61e50f9106b53b196dca35
DannylSyph3r <dopedanny001@gmail.com>
2025-12-28 12:57:12 +0100

    Chore: Initialize Project

---
+++ src/main/java/dev/slethware/hermez/HermezApplication.java
@@ -0,0 +1,13 @@
+package dev.slethware.hermez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/slethware/hermez/HermezApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.slethware.hermez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
