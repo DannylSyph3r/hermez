297b4c311c51fad56a2e5cd540f805d6d0b181fb
DannylSyph3r <dopedanny001@gmail.com>
2025-12-29 01:22:00 +0100

    Fix: Exception Class fix and add README

--- src/main/java/dev/slethware/hermez/exception/InternalServerException.java
+++ src/main/java/dev/slethware/hermez/exception/InternalServerException.java
@@ -1,7 +1,22 @@
 package dev.slethware.hermez.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@Setter
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class InternalServerException extends RuntimeException {
+
+    private HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
     public InternalServerException(String message) {
         super(message);
+    }
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
