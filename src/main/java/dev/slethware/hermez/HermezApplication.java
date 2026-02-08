66c99823944fc05a442fb44ae00463e68edf2a2e
DannylSyph3r <dopedanny001@gmail.com>
2026-02-08 22:36:51 +0100

    Feat: Add Bearer Auth Security requirement to MainApp Class

--- src/main/java/dev/slethware/hermez/HermezApplication.java
+++ src/main/java/dev/slethware/hermez/HermezApplication.java
@@ -1,9 +1,13 @@
 package dev.slethware.hermez;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,14 +18,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 						email = "slethware@gmail.com",
 						url = "https://slethware.dev"
 				),
-				description = "Hermez - Open Source Tunneling Platform",
+				description = "Hermez Tunneling Service API - Expose your localhost to the internet",
 				title = "Hermez API Documentation",
 				version = "1.0",
 				license = @License(
 						name = "MIT License",
 						url = "https://opensource.org/licenses/MIT"
 				)
-		)
+		),
+		security = {
+                 @SecurityRequirement(
+		                  name = "bearerAuth"
+                 )
+		}
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		description = "JWT Bearer Token Authentication",
+		scheme = "bearer",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		in = SecuritySchemeIn.HEADER
 )
 @SpringBootApplication
 public class HermezApplication {
